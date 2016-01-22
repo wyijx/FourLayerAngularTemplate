ab413991fc0c07782c1a417dc67e96de42978816
wyijx <jack.n.hutson@gmail.com>
2016-01-22 13:39:29 -0600

    initial commit

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using $safeprojectname$.Domain;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace $safeprojectname$.Infrastructure {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false) {
+        }
+
+        public static ApplicationDbContext Create() {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Presentation/ngApp/app.ts
@@ -0,0 +1,8 @@
+﻿namespace $safeprojectname$ {
+    angular.module('$safeprojectname$', ['ngRoute', 'ui.bootstrap'])
+        .config(function ($routeProvider: ng.route.IRouteProvider) {
+            $routeProvider.when('/', {
+                template: 'Hello World!'
+            });
+        });
+}
